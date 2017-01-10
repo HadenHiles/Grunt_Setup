0dd6a846da7b9f53a42040cbff1454ffb4f99be9
haden <haden@geekpower.ca>
2017-01-10 08:34:38 -0500

    use grunt-contrib-clean force option to ensure cleaning of build files even if grunt is not executed from the same directory

--- grunt/clean.js
+++ grunt/clean.js
@@ -2,6 +2,9 @@
  * Created by haden on 1/6/2017.
  */
 module.exports = {
+    options: {
+        force: true
+    },
     src : [ "assets/css/src/build.less",
         "assets/css/src/build.min.css",
         "assets/js/src/build.min.js"
